7c88feb8c311ef8a017e9a9841ce39fcb3be85eb
mkubdev <mkubdev@gmail.com>
2023-08-07 14:15:32 +0200

    clean: css and remove unused import on app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,3 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 // Import Swiper React components
@@ -12,7 +9,6 @@ import 'swiper/css';
 import 'swiper/css/navigation'
 
 function App () {
-  const [count, setCount] = useState(0)
 
   return (
     <>
